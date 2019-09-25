004a4e162d81c82dc80809824028ffb8fa0ed2a9
baileyk2 <46055645+baileyk2@users.noreply.github.com>
2019-09-24 20:40:24 -0400

    Collection should be plural elections

--- SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/ElectionRepository.java
+++ SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/ElectionRepository.java
@@ -3,5 +3,5 @@ package src.main.java.com.okta.springbootvue.SpringBootVueApplication;
 import org.springframework.data.jpa.repository.JpaRepository;  
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;  
   
-@RepositoryRestResource(collectionResourceRel = "election", path = "election")
+@RepositoryRestResource(collectionResourceRel = "elections", path = "election")
 interface ElectionRepository extends JpaRepository<Election, Long> {}

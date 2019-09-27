0dfe60ebe0b6b839abd67e8cd8707683076732de
baileyk2 <46055645+baileyk2@users.noreply.github.com>
2019-09-27 14:04:06 -0400

    Added Election CRUD operations

--- SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Controller/ElectionController.java
+++ SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Controller/ElectionController.java
@@ -2,6 +2,18 @@ package src.main.java.com.okta.springbootvue.SpringBootVueApplication.Controller
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,65 +23,112 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.Election;
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.User;
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Dao.ElectionConnectionDao;
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Service.ElectionService;
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Service.UserService;
 
 @RestController
 public class ElectionController {
+	
+	@Autowired
+	ElectionService electionService;
+	
+	@Autowired
+	UserService userService;
 
-  private final ElectionRepository repository;
-
-  ElectionController(ElectionRepository repository) {
-    this.repository = repository;
+  //Return all existing elections
+  @GetMapping("/election")
+  public ResponseEntity<List<Election>> listAllElections() {
+      List<Election> elections = electionService.findAllElections();
+      if(elections.isEmpty()){
+          return new ResponseEntity<List<Election>>(HttpStatus.NO_CONTENT);
+      }
+      return new ResponseEntity<List<Election>>(elections, HttpStatus.OK);
   }
+  
+//Return Single Election by electionID
+  @GetMapping("/election/{electionID}")
+  public ResponseEntity<Election> getElection(@PathVariable("electionID") int electionID) {
+      Election election = electionService.findById(electionID);
+      if (election == null) {
+          System.out.println("Election with id " + electionID + " not found");
+          return new ResponseEntity<Election>(HttpStatus.NOT_FOUND);
+      }
+      return new ResponseEntity<Election>(election, HttpStatus.OK);
+  }
+  
+  //Add New Election
+  @PostMapping("/election/addElection")
+  public Election newElection(@RequestBody Election election) {
 
-  //Get all elections
-  @GetMapping("/elections")
-  List<Election> all() {
-    return repository.findAll();
+      electionService.addElection(election);
+      return election;
   }
 
   //Modify Existing Election
-  @PutMapping("/elections/modify/{electionId}")
-  Election replaceElection(@RequestBody Election newElection, @PathVariable Long electionId) {
-
-	return repository.findById(electionId)
-		.map(election -> {
-		   election.setTitle(newElection.getTitle());
-		   election.setClosed(newElection.getClosed());
-		   election.setNumVotes(newElection.getNumVotes());
-		   election.setNumCandidates(newElection.getNumCandidates());
-		   election.setCloseTime(newElection.getCloseTime());
-		   election.setCloseDate(newElection.getCloseDate());
-		   election.setAdmin1(newElection.getAdmin1());
-		   election.setAdmin2(newElection.getAdmin2());
-		   election.setAdmin3(newElection.getAdmin3());
-		   election.setAdmin4(newElection.getAdmin4());
-		   election.setAdmin5(newElection.getAdmin5());
-		   election.setAdmin6(newElection.getAdmin6());
-		   return repository.save(election);
-	   })
-	   .orElseGet(() -> {
-	      newElection.setElectionId(electionId);
-	      return repository.save(newElection);
-	   });
- }
-
-  //Get one election
- @GetMapping("/elections/{electionId}")
- Election one(@PathVariable Long electionId) {
-
-   return repository.findById(electionId)
-     .orElseThrow(() -> new ElectionNotFoundException(electionId));
- }
-    
-  //Add New Election
-  @PostMapping("/elections/addElection")
-  Election newElection(@RequestBody Election newElection) {
-	  return repository.save(newElection);
+  @PutMapping("/election/modifyElection/{electionID}")
+  public ResponseEntity<Election> modifyElection(@RequestBody Election election, @PathVariable int electionID) {
+	  
+	  Election currentElection = electionService.findById(electionID);
+	  
+	  if (currentElection==null) {
+          System.out.println("Election with id " + electionID + " not found");
+          return new ResponseEntity<Election>(HttpStatus.NOT_FOUND);
+      }  
+	  
+	  currentElection.setElectionID(election.getElectionID());
+	  currentElection.setTitle(election.getTitle());
+	  currentElection.setClosed(election.getClosed());
+	  currentElection.setAdmin1(election.getAdmin1());
+	  currentElection.setAdmin2(election.getAdmin2());
+	  currentElection.setAdmin3(election.getAdmin3());
+	  currentElection.setAdmin4(election.getAdmin4());
+	  currentElection.setAdmin5(election.getAdmin5());
+	  currentElection.setAdmin6(election.getAdmin6());
+	  currentElection.setChoice1(election.getChoice1());
+	  currentElection.setChoice2(election.getChoice2());
+	  currentElection.setChoice3(election.getChoice3());
+	  currentElection.setChoice4(election.getChoice4());
+	  currentElection.setChoice5(election.getChoice5());
+	  currentElection.setClose_date(election.getClose_date());
+	  currentElection.setClose_time(election.getClose_time());
+	  currentElection.setNum_candidates(election.getNum_candidates());
+	  currentElection.setNum_votes(election.getNum_votes());
+	  currentElection.setStart_date(election.getStart_date());
+	  currentElection.setStart_time(election.getStart_time());
+       
+      electionService.updateElection(currentElection);
+      return new ResponseEntity<Election>(currentElection, HttpStatus.OK);
+	    
   }
-
-  //Remove one election
-  @DeleteMapping("/elections/remove/{electionId}")
-  void deleteElection(@PathVariable Long electionId) {
-    repository.deleteById(electionId);
+  
+  //Remove Existing Election
+  @DeleteMapping("/user/removeElection/{electionID}")
+  public ResponseEntity<Election> deleteUser(@PathVariable("electionID") int electionID) {
+	  Election election = electionService.findById(electionID);
+	  if (election == null) {
+	      System.out.println("Unable to delete. Election with id " + electionID + " not found");
+	      return new ResponseEntity<Election>(HttpStatus.NOT_FOUND);
+	  }
+	 
+	  electionService.deleteElectionById(electionID);
+	  return new ResponseEntity<Election>(HttpStatus.NO_CONTENT);
   }
+  
+  //Associate a Voter
+  @PostMapping("/election/associateVoter/{electionID}/{id}")
+  public ResponseEntity<Election> getElection(@PathVariable("electionID") int electionID, @PathVariable("id") String id){
+	  Election election = electionService.findById(electionID);
+	  User user = userService.findById(id);
+	  
+	  if (election == null || user == null) {
+          System.out.println("Election or User not found.");
+          return new ResponseEntity<Election>(HttpStatus.NOT_FOUND);
+      }
+
+      electionService.associateVoter(electionID, id);
+      return new ResponseEntity<Election>(HttpStatus.NO_CONTENT);
+  }
+  
 }

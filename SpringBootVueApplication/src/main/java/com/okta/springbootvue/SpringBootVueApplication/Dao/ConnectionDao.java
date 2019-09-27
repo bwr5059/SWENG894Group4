8323f219171a69189000b58659813b570fdbb144
baileyk2 <46055645+baileyk2@users.noreply.github.com>
2019-09-27 07:45:09 -0400

    Adapted to DB Changes
    User Table changes

--- SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Dao/ConnectionDao.java
+++ SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Dao/ConnectionDao.java
@@ -31,20 +31,20 @@ public class ConnectionDao {
 		ResultSet rs=stmt.executeQuery("select * from user");  
 		while(rs.next())  {
 			User user = new User();
-			user.setId(rs.getLong(1));
+			user.setId(rs.getString(1));
 			user.setEmail(rs.getString(2));
-			user.setFirstName(rs.getString(3));
-			user.setLastName(rs.getString(4));
-			user.setType(rs.getString(5));
-			user.setAge(rs.getInt(6));
-			user.setEthnicity(rs.getString(7));
-			user.setRace(rs.getString(8));
+			user.setUser_name(rs.getString(3));
+			user.setFirst_name(rs.getString(4));
+			user.setLast_name(rs.getString(5));
+			user.setType(rs.getString(6));
+			user.setAge(rs.getInt(7));
+			user.setEthnicity(rs.getString(8));
 			user.setGender(rs.getString(9));
-			user.setProfileComplete(rs.getInt(10));
+			user.setProfile_complete(rs.getInt(10));
 			user.setAddress(rs.getString(11));
 			user.setCity(rs.getString(12));
 			user.setState(rs.getString(13));
-			user.setZip(rs.getInt(14));
+			user.setZip(rs.getString(14));
 			userList.add(user);
 		}
 		System.out.println("list is: "+userList.size());
@@ -62,23 +62,23 @@ public class ConnectionDao {
 		}
 
 		try {
-		String sql = "INSERT INTO user (id, email, first_name, last_name, type, age, ethnicity, race, gender, profile_complete, " +
+		String sql = "INSERT INTO user (id, email, user_name, first_name, last_name, type, age, ethnicity, gender, profile_complete, " +
 				"address, city, state, zip) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 		PreparedStatement stmt=conn.prepareStatement(sql);
-		stmt.setLong(1,user.getId());
+		stmt.setString(1,user.getId());
 		stmt.setString(2,user.getEmail());
-		stmt.setString(3,user.getFirstName());
-		stmt.setString(4,user.getLastName());
-		stmt.setString(5,user.getType());
-		stmt.setInt(6,user.getAge());
-		stmt.setString(7,user.getEthnicity());
-		stmt.setString(8,user.getRace());
+		stmt.setString(3,user.getUser_name());
+		stmt.setString(4,user.getFirst_name());
+		stmt.setString(5,user.getLast_name());
+		stmt.setString(6,user.getType());
+		stmt.setInt(7,user.getAge());
+		stmt.setString(8,user.getEthnicity());
 		stmt.setString(9,user.getGender());
-		stmt.setInt(10,user.getProfileComplete());
+		stmt.setInt(10,user.getProfile_complete());
 		stmt.setString(11,user.getAddress());
 		stmt.setString(12,user.getCity());
 		stmt.setString(13,user.getState());
-		stmt.setInt(14,user.getZip());
+		stmt.setString(14,user.getZip());
 		
 		stmt.executeUpdate();  
 		

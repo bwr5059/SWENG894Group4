88de7fa063adc05484f53a03495d8a20962f3597
baileyk2 <46055645+baileyk2@users.noreply.github.com>
2019-09-27 09:59:39 -0400

    User CRUD Updates

--- SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Dao/ConnectionDao.java
+++ SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Dao/ConnectionDao.java
@@ -33,18 +33,19 @@ public class ConnectionDao {
 			User user = new User();
 			user.setId(rs.getString(1));
 			user.setEmail(rs.getString(2));
-			user.setUser_name(rs.getString(3));
-			user.setFirst_name(rs.getString(4));
-			user.setLast_name(rs.getString(5));
-			user.setType(rs.getString(6));
-			user.setAge(rs.getInt(7));
-			user.setEthnicity(rs.getString(8));
-			user.setGender(rs.getString(9));
-			user.setProfile_complete(rs.getInt(10));
-			user.setAddress(rs.getString(11));
-			user.setCity(rs.getString(12));
-			user.setState(rs.getString(13));
-			user.setZip(rs.getString(14));
+			user.setType(rs.getString(3));
+			user.setAge(rs.getInt(4));
+			user.setEthnicity(rs.getString(5));
+			user.setGender(rs.getString(6));
+			user.setAddress(rs.getString(7));
+			user.setCity(rs.getString(8));
+			user.setState(rs.getString(9));
+			user.setZip(rs.getString(10));
+			user.setFirst_name(rs.getString(11));
+			user.setLast_name(rs.getString(12));
+			user.setProfile_complete(rs.getInt(13));
+			user.setUser_name(rs.getString(14));
+			user.setRace(rs.getString(15));
 			userList.add(user);
 		}
 		System.out.println("list is: "+userList.size());
@@ -62,44 +63,83 @@ public class ConnectionDao {
 		}
 
 		try {
-		String sql = "INSERT INTO user (id, email, user_name, first_name, last_name, type, age, ethnicity, gender, profile_complete, " +
-				"address, city, state, zip) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+		String sql = "INSERT INTO user (id, email, type, age, ethnicity, gender, address, city, state, zip, first_name, " +
+				"last_name, profile_complete, user_name, race) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 		PreparedStatement stmt=conn.prepareStatement(sql);
+		
 		stmt.setString(1,user.getId());
 		stmt.setString(2,user.getEmail());
-		stmt.setString(3,user.getUser_name());
-		stmt.setString(4,user.getFirst_name());
-		stmt.setString(5,user.getLast_name());
-		stmt.setString(6,user.getType());
-		stmt.setInt(7,user.getAge());
-		stmt.setString(8,user.getEthnicity());
-		stmt.setString(9,user.getGender());
-		stmt.setInt(10,user.getProfile_complete());
-		stmt.setString(11,user.getAddress());
-		stmt.setString(12,user.getCity());
-		stmt.setString(13,user.getState());
-		stmt.setString(14,user.getZip());
-		
+		stmt.setString(3,user.getType());
+		stmt.setInt(4,user.getAge());
+		stmt.setString(5,user.getEthnicity());
+		stmt.setString(6,user.getGender());
+		stmt.setString(7,user.getAddress());
+		stmt.setString(8,user.getCity());
+		stmt.setString(9,user.getState());
+		stmt.setString(10,user.getZip());
+		stmt.setString(11,user.getFirst_name());
+		stmt.setString(12,user.getLast_name());
+		stmt.setInt(13,user.getProfile_complete());
+		stmt.setString(14,user.getUser_name());
+		stmt.setString(15,user.getRace());
 		stmt.executeUpdate();  
 		
-		/*User userNew = new User();
-		userNew.setId(rs.getLong(1));
-		userNew.setEmail(rs.getString(2));
-		userNew.setFirstName(rs.getString(3));
-		userNew.setLastName(rs.getString(4));
-		userNew.setType(rs.getString(5));
-		userNew.setAge(rs.getInt(6));
-		userNew.setEthnicity(rs.getString(7));
-		userNew.setRace(rs.getString(8));
-		userNew.setGender(rs.getString(9));
-		userNew.setProfileComplete(rs.getBoolean(10));
-		userNew.setAddress(rs.getString(11));
-		userNew.setCity(rs.getString(12));
-		userNew.setState(rs.getString(13));
-		userNew.setZip(rs.getInt(14));
-		userList.add(user);*/
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return userList;
+		//con.close();  
 		
-		System.out.println("list is: "+userList.size());
+	}
+	
+	public List<User> updateUser(Connection conn, User user, List<User> userList){
+		if(conn == null) {
+			conn = RetriveConnection();
+		}
+
+		try {
+		String sql = "UPDATE user SET id=?, email=?, type=?, age=?, ethnicity=?, gender=?, address=?, city=?, state=?, zip=?, first_name=?, " +
+				"last_name=?, profile_complete=?, user_name=?, race=? WHERE id=?";
+		PreparedStatement stmt=conn.prepareStatement(sql);
+		
+		stmt.setString(1,user.getId());
+		stmt.setString(2,user.getEmail());
+		stmt.setString(3,user.getType());
+		stmt.setInt(4,user.getAge());
+		stmt.setString(5,user.getEthnicity());
+		stmt.setString(6,user.getGender());
+		stmt.setString(7,user.getAddress());
+		stmt.setString(8,user.getCity());
+		stmt.setString(9,user.getState());
+		stmt.setString(10,user.getZip());
+		stmt.setString(11,user.getFirst_name());
+		stmt.setString(12,user.getLast_name());
+		stmt.setInt(13,user.getProfile_complete());
+		stmt.setString(14,user.getUser_name());
+		stmt.setString(15,user.getRace());
+		stmt.setString(16,user.getId());
+		stmt.executeUpdate();  
+		
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return userList;
+		//con.close();  
+		
+	}
+	
+	public List<User> deleteUser(Connection conn, String Id, List<User> userList){
+		if(conn == null) {
+			conn = RetriveConnection();
+		}
+
+		try {
+		String sql = "DELETE FROM user WHERE id=?";
+		PreparedStatement stmt=conn.prepareStatement(sql);
+		
+		stmt.setString(1,Id);
+		stmt.executeUpdate();  
+		
 		}catch(Exception e) {
 			e.printStackTrace();
 		}

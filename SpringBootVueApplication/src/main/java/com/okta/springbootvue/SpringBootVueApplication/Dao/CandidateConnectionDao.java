ea5c414f895a068c87398bd5b503634b4cf4b6f4
baileyk2 <46055645+baileyk2@users.noreply.github.com>
2019-10-02 14:45:14 -0400

    Add Candidate and Connection

---
+++ SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Dao/CandidateConnectionDao.java
@@ -0,0 +1,84 @@
+package src.main.java.com.okta.springbootvue.SpringBootVueApplication.Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.PreparedStatement;
+
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.Candidate;
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.User;
+
+public class CandidateConnectionDao {
+	ConnectionDao connectionDao = new ConnectionDao();
+	
+	/**
+	 * getCandidateList() - Performs select MySQL statement to retrieve all candidates from candidate table.
+	 * @param conn
+	 * @return List<User>
+	 */
+	public List<Candidate> getCandidateList(){
+		List<Candidate> candidateList = new ArrayList<>();
+		try {
+			Connection conn = connectionDao.RetrieveConnection();
+			
+			Statement stmt=conn.createStatement();  
+			ResultSet rs=stmt.executeQuery("select * from candidate");  
+			while(rs.next())  {
+				Candidate candidate = new Candidate();
+				candidate.setCanID(rs.getInt(1));
+				candidate.setUserID(rs.getString(2));
+				candidate.setFirst_name(rs.getString(3));
+				candidate.setLast_name(rs.getString(4));
+				candidate.setEmail(rs.getString(5));
+				candidate.setElectionID(rs.getInt(6));
+				candidate.setAbout(rs.getString(7));
+				candidate.setEducation(rs.getString(8));
+				candidate.setEmployment(rs.getString(9));
+				candidate.setExperience(rs.getString(10));
+				candidate.setContact(rs.getString(11));
+				candidateList.add(candidate);
+			}
+			connectionDao.ReleaseConnection(conn);
+			System.out.println("list is: "+candidateList.size());
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		return candidateList;
+	}
+	
+	public void insertCandidate(Candidate candidate){
+		try {
+			Connection conn = connectionDao.RetrieveConnection();
+		String sql = "INSERT INTO vision_database.candidate (canID, userID, first_name, last_name, email, electionID, about, education, employment, experience, contact) " +
+				"VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+		/*String sql = "INSERT INTO vision_database.candidate (canID) " +
+				"VALUES (?)";*/
+		PreparedStatement stmt=conn.prepareStatement(sql);
+		
+		
+		//stmt.setInt(1,candidate.getCanID());
+		
+		stmt.setInt(1,candidate.getCanID());
+		stmt.setString(2,candidate.getUserID());
+		stmt.setString(3,candidate.getFirst_name());
+		stmt.setString(4,candidate.getLast_name());
+		stmt.setString(5,candidate.getEmail());
+		stmt.setInt(6,candidate.getElectionID());
+		stmt.setString(7,candidate.getAbout());
+		stmt.setString(8,candidate.getEducation());
+		stmt.setString(9,candidate.getEmployment());
+		stmt.setString(10,candidate.getExperience());
+		stmt.setString(11,candidate.getContact());
+		stmt.executeUpdate();  
+		connectionDao.ReleaseConnection(conn);
+		
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
--- SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Dao/ConnectionDao.java
+++ SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Dao/ConnectionDao.java
@@ -1,3 +1,12 @@
+/*---------------------------------------------------------------------
+|  Class ConnectionDao
+|
+|  Purpose: Establishes and Closes Database Connections
+|
+|  Version: Sprint 1
+|  
+*-------------------------------------------------------------------*/
+
 package src.main.java.com.okta.springbootvue.SpringBootVueApplication.Dao;
 
 import java.sql.Connection;
@@ -10,160 +19,39 @@ import java.sql.PreparedStatement;
 
 import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.User;
 
+/**
+ * ConnectionDao Class - Connects to MySQL database vision-database
+ */
 public class ConnectionDao {
-	Connection con = null;
-	public Connection RetriveConnection() {
-	try {
-		Class.forName("com.mysql.jdbc.Driver");  
-		con=DriverManager.getConnection(  
-		"jdbc:mysql://vision-database.cmhohrk4u5fw.us-east-2.rds.amazonaws.com:3306/vision_database","admin","visionelection19");
-		}catch(Exception e){ System.out.println(e);}
-	return con;  
+	//Connection con = null;
+	
+	/**
+	 * RetrieveConnection() - Establishes and returns a connection to the MYSQL database.
+	 * @return con
+	 */
+	public Connection RetrieveConnection() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");  
+			con=DriverManager.getConnection(  
+					"jdbc:mysql://vision-database.cmhohrk4u5fw.us-east-2.rds.amazonaws.com:3306/vision_database","admin","visionelection19");
+			}catch(Exception e){ 
+				System.out.println(e);
+			}
+		return con;  
 	}
 	
-	public List<User> getUserList(Connection conn){
-		List<User> userList = new ArrayList<>();
+	/**
+	 * ReleaseConnection() - Closes connection to the MYSQL database.
+	 * @return con
+	 */
+	public void ReleaseConnection(Connection con) {
 		try {
-			if(conn == null) {
-				conn = RetriveConnection();
-			}
-		Statement stmt=conn.createStatement();  
-		ResultSet rs=stmt.executeQuery("select * from user");  
-		while(rs.next())  {
-			User user = new User();
-			user.setId(rs.getString(1));
-			user.setEmail(rs.getString(2));
-			user.setType(rs.getString(3));
-			user.setAge(rs.getInt(4));
-			user.setEthnicity(rs.getString(5));
-			user.setGender(rs.getString(6));
-			user.setAddress(rs.getString(7));
-			user.setCity(rs.getString(8));
-			user.setState(rs.getString(9));
-			user.setZip(rs.getString(10));
-			user.setFirst_name(rs.getString(11));
-			user.setLast_name(rs.getString(12));
-			user.setProfile_complete(rs.getInt(13));
-			user.setUser_name(rs.getString(14));
-			user.setRace(rs.getString(15));
-			userList.add(user);
+			con.close();
 		}
-		conn.close(); 
-		System.out.println("list is: "+userList.size());
-		}catch(Exception e) {
-			e.printStackTrace();
+		catch(Exception e) {
+			System.out.println(e);
 		}
-		return userList;
-		
-	}
-	
-	public List<User> insertUser(Connection conn, User user, List<User> userList){
-		try {
-			if(conn == null) {
-				conn = RetriveConnection();
-			}
-		String sql = "INSERT INTO user (id, email, type, age, ethnicity, gender, address, city, state, zip, first_name, " +
-				"last_name, profile_complete, user_name, race) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-		PreparedStatement stmt=conn.prepareStatement(sql);
-		
-		stmt.setString(1,user.getId());
-		stmt.setString(2,user.getEmail());
-		stmt.setString(3,user.getType());
-		stmt.setInt(4,user.getAge());
-		stmt.setString(5,user.getEthnicity());
-		stmt.setString(6,user.getGender());
-		stmt.setString(7,user.getAddress());
-		stmt.setString(8,user.getCity());
-		stmt.setString(9,user.getState());
-		stmt.setString(10,user.getZip());
-		stmt.setString(11,user.getFirst_name());
-		stmt.setString(12,user.getLast_name());
-		stmt.setInt(13,user.getProfile_complete());
-		stmt.setString(14,user.getUser_name());
-		stmt.setString(15,user.getRace());
-		stmt.executeUpdate();  
-		conn.close(); 
-		
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		return userList;
-		
-	}
-	
-	public List<User> updateUser(Connection conn, User user, List<User> userList){
-		try {
-			if(conn == null) {
-				conn = RetriveConnection();
-			}
-		String sql = "UPDATE user SET id=?, email=?, type=?, age=?, ethnicity=?, gender=?, address=?, city=?, state=?, zip=?, first_name=?, " +
-				"last_name=?, profile_complete=?, user_name=?, race=? WHERE id=?";
-		PreparedStatement stmt=conn.prepareStatement(sql);
-		
-		stmt.setString(1,user.getId());
-		stmt.setString(2,user.getEmail());
-		stmt.setString(3,user.getType());
-		stmt.setInt(4,user.getAge());
-		stmt.setString(5,user.getEthnicity());
-		stmt.setString(6,user.getGender());
-		stmt.setString(7,user.getAddress());
-		stmt.setString(8,user.getCity());
-		stmt.setString(9,user.getState());
-		stmt.setString(10,user.getZip());
-		stmt.setString(11,user.getFirst_name());
-		stmt.setString(12,user.getLast_name());
-		stmt.setInt(13,user.getProfile_complete());
-		stmt.setString(14,user.getUser_name());
-		stmt.setString(15,user.getRace());
-		stmt.setString(16,user.getId());
-		stmt.executeUpdate();  
-		conn.close();
-		
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		return userList;
-		
-	}
-	
-	public void updateUserType(Connection conn, String id, String type){
-		try {
-			if(conn == null) {
-				conn = RetriveConnection();
-			}
-		String sql = "UPDATE user SET type=? WHERE id=?";
-		PreparedStatement stmt=conn.prepareStatement(sql);
-		
-		stmt.setString(1,type);
-		stmt.setString(2,id);
-		stmt.executeUpdate(); 
-		
-		conn.close();  
-		
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public List<User> deleteUser(Connection conn, String Id, List<User> userList){
-		try {
-			if(conn == null) {
-				conn = RetriveConnection();
-			}
-		String sql = "DELETE FROM user WHERE id=?";
-		PreparedStatement stmt=conn.prepareStatement(sql);
-		
-		stmt.setString(1,Id);
-		stmt.executeUpdate(); 
-		conn.close(); 
-		
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		return userList;
-		
 	}
 
 }

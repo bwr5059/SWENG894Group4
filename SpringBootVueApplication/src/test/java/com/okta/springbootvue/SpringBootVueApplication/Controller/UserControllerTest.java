c1e9196979564fc0f72c65eee2fd7081daf597e1
bwr5059 <55011215+bwr5059@users.noreply.github.com>
2019-10-13 15:17:19 -0400

    Continued Controller Unit tests

--- SpringBootVueApplication/src/test/java/com/okta/springbootvue/SpringBootVueApplication/Controller/UserControllerTest.java
+++ SpringBootVueApplication/src/test/java/com/okta/springbootvue/SpringBootVueApplication/Controller/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.okta.springbootvue.SpringBootVueApplication.Controller;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -10,13 +11,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.User;
 import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Controller.UserController;
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.User;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+
 
 // @RunWith attaches a runner with the test class to initialize the test data
 @RunWith(SpringRunner.class)
@@ -55,33 +57,17 @@ public class UserControllerTest {
         user.setRace("White");
 
 
-        User user2 = new User();
-
-        user2.setId("test");
-        user2.setType("Voter");
-        user2.setAge(25);
-        user2.setEthnicity("European");
-        user2.setGender("F");
-        user2.setAddress("160 Temp Drive");
-        user2.setCity("Philadelphia");
-        user2.setState("PA");
-        user2.setZip("12345");
-        user2.setFirst_name("Han");
-        user2.setLast_name("Solo");
-        user2.setProfile_complete(0);
-        user2.setUser_name("sw1");
-        user2.setRace("White");
-
-        List<User> userList = null;
-        userList.add(user);
-        userList.add(user2);
+        List<User> userList1 = null;
+        userList1.add(user);
 
 
+        User user2 = UserController.newUser(user,"Voter");
 
+        assertEquals(user.getID, user2.getID);
 
+        List<User> users = userService.findAllUsers();
 
-
-
+        assertEquals(userList1.size(),users.size());
 
 
     }
@@ -114,6 +100,15 @@ public class UserControllerTest {
         testList.add(user);
 
         when(userConnectionDAO.insertUser(user,testList)).thenReturn(testList);
+
+        userService.addUser(user,"Voter");
+
+        String ID = user.getID();
+
+        User user2 = userService.findById(ID);
+
+        assertEquals(user.getID(),user2.getID());
+        assertEquals(user.getType(),user2.getType());
 
 
     }
@@ -161,6 +156,34 @@ public class UserControllerTest {
 
     @Test
     public void modifyUser() {
+
+        User user = new User();
+
+        user.setId("test");
+        user.setType("Voter");
+        user.setAge(25);
+        user.setEthnicity("European");
+        user.setGender("F");
+        user.setAddress("160 Temp Drive");
+        user.setCity("Philadelphia");
+        user.setState("PA");
+        user.setZip("12345");
+        user.setFirst_name("Han");
+        user.setLast_name("Solo");
+        user.setProfile_complete(0);
+        user.setUser_name("sw1");
+        user.setRace("White");
+
+        String ID = user.getID();
+
+        User user2 = UserController.newUser(user,"Voter");
+
+
+
+
+
+
+
     }
 
     @Test

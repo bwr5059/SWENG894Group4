98c7711f418aeed109f4318670e7227d0d822c00
bwr5059 <55011215+bwr5059@users.noreply.github.com>
2019-10-24 21:55:49 -0400

    Cleanup deletes

--- SWENG894Group4-Kylie/SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Controller/ElectionNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package src.main.java.com.okta.springbootvue.SpringBootVueApplication.Controller;
-
-
-class ElectionNotFoundException extends RuntimeException {
-
-  ElectionNotFoundException(Long electionId) {
-    super("Could not find election " + electionId);
-  }
-}

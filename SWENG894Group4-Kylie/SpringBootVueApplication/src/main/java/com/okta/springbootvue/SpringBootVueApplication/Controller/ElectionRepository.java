549f98aae770ad5c1482ee536a050a3d29039cec
bwr5059 <55011215+bwr5059@users.noreply.github.com>
2019-10-24 21:55:37 -0400

    Cleanup deletes

--- SWENG894Group4-Kylie/SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Controller/ElectionRepository.java
+++
@@ -1,9 +0,0 @@
-package src.main.java.com.okta.springbootvue.SpringBootVueApplication.Controller; 
-
-import org.springframework.data.jpa.repository.JpaRepository;  
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.Election;  
-  
-@RepositoryRestResource(collectionResourceRel = "elections", path = "election")
-interface ElectionRepository extends JpaRepository<Election, Long> {}

9ff44966bcee37ca1497feff8dbb35f5fa2a8a15
bwr5059 <55011215+bwr5059@users.noreply.github.com>
2019-10-24 21:54:55 -0400

    Cleanup deletes

--- SWENG894Group4-Kylie/SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Controller/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package src.main.java.com.okta.springbootvue.SpringBootVueApplication.Controller; 
-
-import org.springframework.data.jpa.repository.JpaRepository;  
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.User;  
-  
-@RepositoryRestResource(collectionResourceRel = "users", path = "user")
-interface UserRepository extends JpaRepository<User, Long> {}

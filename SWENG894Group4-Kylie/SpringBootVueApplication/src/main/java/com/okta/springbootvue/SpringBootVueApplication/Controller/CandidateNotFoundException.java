6ed12403b74ef6f541c657d4138ff29df021576e
bwr5059 <55011215+bwr5059@users.noreply.github.com>
2019-10-24 21:56:24 -0400

    Cleanup deletes

--- SWENG894Group4-Kylie/SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Controller/CandidateNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package src.main.java.com.okta.springbootvue.SpringBootVueApplication.Controller;
-
-
-class CandidateNotFoundException extends RuntimeException {
-
-  CandidateNotFoundException(Long id) {
-    super("Could not find user " + id);
-  }
-}

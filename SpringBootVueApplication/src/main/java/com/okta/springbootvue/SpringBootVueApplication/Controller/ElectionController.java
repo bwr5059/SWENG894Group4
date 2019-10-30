ce1e40f9eab41317417f6d5468af80d88b070026
baileyk2 <46055645+baileyk2@users.noreply.github.com>
2019-10-29 20:11:05 -0400

    set fix

--- SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Controller/ElectionController.java
+++ SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Controller/ElectionController.java
@@ -127,7 +127,7 @@ public class ElectionController {
 		currentElection.setStart_date(election.getStart_date());
 		currentElection.setStart_time(election.getStart_time());
 		currentElection.setDescription(election.getDescription());
-		currentElection.setKey(election.getElection_key());
+		currentElection.setElection_key(election.getElection_key());
        
 		electionService.updateElection(currentElection);
 		return new ResponseEntity<Election>(currentElection, HttpStatus.OK);

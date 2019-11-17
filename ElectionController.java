d56ec60d2f81ca64bb97259384cd264c46280777
bwr5059 <55011215+bwr5059@users.noreply.github.com>
2019-11-17 00:00:34 -0500

    Set Election Closed

---
+++ ElectionController.java
@@ -0,0 +1,386 @@
+/*---------------------------------------------------------------------
+|  Class ElectionController
+|
+|  Purpose: Set up Election REST Endpoints
+|
+|  Methods: listAllElections, getElection, newElection, modifyElection,
+|           deleteElection, associateVoter, associateCandidate, 
+|           removeCandidate, viewCandidates
+|
+|  Version: Sprint 2
+|  
+*-------------------------------------------------------------------*/
+package src.main.java.com.okta.springbootvue.SpringBootVueApplication.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.Election;
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.User;
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.Candidate;
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.Policy;
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Dao.ElectionConnectionDao;
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Service.ElectionService;
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Service.UserService;
+
+/**
+ * Class ElectionController - Returns election object and data using CRUD methods.
+ */
+@RestController
+public class ElectionController {
+	
+	@Autowired
+	ElectionService electionService;
+	
+	@Autowired
+	UserService userService;
+
+	/**
+     * listAllElections() - Calls findAllElections() method in electionService class. Returns election list response of
+     * elections if not null.
+     * @return new ResponseEntity<List<Election>>(elections, HttpStatus)
+     *
+    **/
+	@GetMapping("/election")
+	public ResponseEntity<List<Election>> listAllElections() {
+		List<Election> elections = electionService.findAllElections();
+		if(elections.isEmpty()){
+			return new ResponseEntity<List<Election>>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<Election>>(elections, HttpStatus.OK);
+	}
+  
+	/**
+	 * getElection () - Calls findByID() method in electionService class, returns response containing election if electionID
+	 * is found in database.
+	 * @param electionID
+	 * @return new ResponseEntity<Election>(election, HttpStatus)
+	**/
+	@GetMapping("/election/{electionID}")
+	public ResponseEntity<Election> getElection(@PathVariable("electionID") int electionID) throws Exception{
+		Election election = electionService.findElectionById(electionID);
+		if (election == null) {
+			return new ResponseEntity<Election>(HttpStatus.NOT_FOUND);
+		}
+
+		//Sets election closed field to 1 if election should be closed
+		electionService.calculateClosed(electionID);
+		return new ResponseEntity<Election>(election, HttpStatus.OK);
+	}
+  
+  /**
+   * newElection() - Takes an election object as a parameter and adds it to the database.
+   * @param election
+   * @return election
+   */
+	@PostMapping("/election/addElection")
+	public ResponseEntity<Election> newElection(@RequestBody Election election) {
+		/*if(userService.findById(election.getElectionID())!=null) {
+			return new ResponseEntity<Election>(HttpStatus.CONFLICT); 
+		}*/
+		
+		electionService.addElection(election);
+		return new ResponseEntity<Election>(election, HttpStatus.CREATED);
+	}
+
+  /**
+   * modifyElection() - Takes an election object and electionID and ,if ID is found in the database, updates election
+   * table fields.
+   * @param election
+   * @param electionID
+   * @return
+   */
+	@PutMapping("/election/modifyElection/{electionID}")
+	public ResponseEntity<Election> modifyElection(@PathVariable int electionID, @RequestBody Election election) {
+		
+		Election currentElection = electionService.findElectionById(electionID);
+	  
+		if (currentElection==null) {
+			return new ResponseEntity<Election>(HttpStatus.NOT_FOUND);
+		}  
+		
+		//Populate currentElection object from submitted object
+		currentElection.setElectionID(electionID);
+		currentElection.setTitle(election.getTitle());
+		currentElection.setClosed(election.getClosed());
+		currentElection.setClose_date(election.getClose_date());
+		currentElection.setClose_time(election.getClose_time());
+		currentElection.setNum_candidates(election.getNum_candidates());
+		currentElection.setNum_votes(election.getNum_votes());
+		currentElection.setStart_date(election.getStart_date());
+		currentElection.setStart_time(election.getStart_time());
+		currentElection.setDescription(election.getDescription());
+		currentElection.setElection_key(election.getElection_key());
+       
+		electionService.updateElection(currentElection);
+		return new ResponseEntity<Election>(currentElection, HttpStatus.OK);
+	    
+	}
+  
+	/**
+	 * deleteElection() - Takes an electionID, searches the database election table for ID, and deletes election if ID is found.
+	 * @param electionID
+	 * @return new ResponseEntity<Election>(HttpStatus)
+	 */
+	@DeleteMapping("/election/removeElection/{electionID}")
+	public ResponseEntity<Election> deleteElection(@PathVariable("electionID") int electionID) {
+		Election election = electionService.findElectionById(electionID);
+		if (election == null) {
+			return new ResponseEntity<Election>(HttpStatus.NOT_FOUND);
+		}
+	 
+		electionService.deleteElectionById(electionID);
+		return new ResponseEntity<Election>(HttpStatus.OK);
+	}
+  
+	/**
+	 * associateVoter() - Takes and electionID and userID. If both values are found in database calls associateVoter method of
+	 * electionService class to add user and election to authorization database table.
+	 * @param electionID
+	 * @param id
+	 * @return
+	 */
+	@PostMapping("/election/associateVoter/{electionID}/{id}")
+	public ResponseEntity<Election> associateVoter(@PathVariable("electionID") int electionID, @PathVariable("id") String id){
+		Election election = electionService.findElectionById(electionID);
+		User user = userService.findById(id);
+	  
+		if (election == null || user == null) {
+			return new ResponseEntity<Election>(HttpStatus.NOT_FOUND);
+		}
+
+		electionService.associateVoter(electionID, id);
+		return new ResponseEntity<Election>(HttpStatus.OK);
+	}
+	
+	/**
+	 * removeVoter() - withdraw voter from registered election
+	 * @param electionID
+	 * @param userID
+	 * @return ResponseEntity <Election>
+	 */
+	@DeleteMapping("/election/withdrawVoter/{electionID}/{id}")
+	public ResponseEntity<Election> removeVoter(@PathVariable("electionID") int electionID, @PathVariable("id") String id){
+		Election election = electionService.findElectionById(electionID);
+		User user = userService.findById(id);
+	  
+		if (election == null || user == null) {
+			return new ResponseEntity<Election>(HttpStatus.NOT_FOUND);
+		}
+
+		electionService.withdrawVoter(electionID, id);
+		return new ResponseEntity<Election>(HttpStatus.OK);
+	}
+	
+	/**
+	 * validateVoter () - Confirm voter is registed for election
+	 * @param electionID
+	 * @param userID
+	 * @return String
+	**/
+	@GetMapping("/election/validateVoter/{electionID}/{id}")
+	public String validateVoter(@PathVariable("electionID") int electionID, @PathVariable("id") String id) {
+		Election election = electionService.findElectionById(electionID);
+		User user = userService.findById(id);
+		
+		if (election == null || user == null) {
+			return "Election or User does not exist";
+		}
+		String result = electionService.validateVoter(electionID, id);
+		return result;
+	}
+  
+	/**
+	 * associateCandidate() - register candidate for an election 
+	 * @param electionID
+	 * @param id
+	 * @return ResponseEntity<Election>
+	 */
+	@PostMapping("/election/associateCandidate/{electionID}/{id}")
+	public ResponseEntity<Election> associateCandidate(@PathVariable("electionID") int electionID, @PathVariable("id") String id){
+		Election election = electionService.findElectionById(electionID);
+		User user = userService.findById(id);
+	  
+		if (election == null || user == null) {
+			return new ResponseEntity<Election>(HttpStatus.NOT_FOUND);
+		}
+
+		electionService.associateCandidate(electionID, id);
+		return new ResponseEntity<Election>(HttpStatus.OK);
+	}
+	
+	/**
+	 * removeCandidate() - Takes an electionID and userID as parameters. If both are found in database, uses withdrawCandidate
+	 * method of the electionService class to delete userID and electionID from candidate database table.
+	 * @param electionID
+	 * @param id
+	 * @return
+	 */
+	@DeleteMapping("/election/withdrawCandidate/{electionID}/{id}")
+	public ResponseEntity<Election> removeCandidate(@PathVariable("electionID") int electionID, @PathVariable("id") String id){
+		Election election = electionService.findElectionById(electionID);
+		User user = userService.findById(id);
+	  
+		if (election == null || user == null) {
+			return new ResponseEntity<Election>(HttpStatus.NOT_FOUND);
+		}
+
+		electionService.withdrawCandidate(electionID, id);
+		return new ResponseEntity<Election>(HttpStatus.OK);
+	}
+	
+	/**
+	 * validateCandidate() - Confirm candidate is registered for an election
+	 * @param electionID
+	 * @param userID
+	 * @return String
+	**/
+	@GetMapping("/election/validateCandidate/{electionID}/{id}")
+	public String validateCandidate(@PathVariable("electionID") int electionID, @PathVariable("id") String id) {
+		Election election = electionService.findElectionById(electionID);
+		User user = userService.findById(id);
+		
+		if (election == null || user == null) {
+			return "Election or User does not exist";
+		}
+		String result = electionService.validateCandidate(electionID, id);
+		return result;
+	}
+	
+	/**
+	 * viewCandidates() - Takes an electionID as a parameter. View candidates by elections.
+	 * @param electionID
+	 * @return
+	 */
+	@GetMapping("/election/viewCandidates/{electionID}")
+	public List<HashMap<String, String>> viewCandidates(@PathVariable("electionID") int electionID){ 
+		Election election = electionService.findElectionById(electionID);
+	  
+		if (election == null) {
+			//return new ResponseEntity<List<Candidate>>(HttpStatus.NOT_FOUND);
+		}
+
+		List<HashMap<String, String>> candidates = electionService.viewCandidates(electionID);
+		//return new ResponseEntity<List<Candidate>>(candidates, HttpStatus.OK);
+		return candidates;
+	}
+	
+	/**
+	 * getPolicy () - View policy of an election
+	 * @param electionID
+	 * @return ResponseEntity<Policy>
+	**/
+	@GetMapping("/election/getPolicy/{electionID}")
+	public ResponseEntity<Policy> getPolicy(@PathVariable("electionID") int electionID) {
+		Election election = electionService.findElectionById(electionID);
+		if (election == null) {
+			return new ResponseEntity<Policy>(HttpStatus.NOT_FOUND);
+		}
+		Policy policy = electionService.getPolicy(electionID);
+		return new ResponseEntity<Policy>(policy, HttpStatus.OK);
+	}
+	
+	/**
+	 * createPolicy() - Add a new policy for an election
+	 * @param Policy
+	 * @return ResponseEntity<Policy>
+	 */
+	@PostMapping("/election/createPolicy")
+	public ResponseEntity<Policy> createPolicy(@RequestBody Policy policy){
+		Election election = electionService.findElectionById(policy.getElectionID());
+	  
+		if (election == null) {
+			return new ResponseEntity<Policy>(HttpStatus.NOT_FOUND);
+		}
+
+		electionService.createPolicy(policy);
+		return new ResponseEntity<Policy>(HttpStatus.OK);
+	}
+	
+	/**
+	 * modifyPolicy() - Modify an existing policy of an election
+	 * @param Policy
+	 * @return ResponseEntity<Policy>
+	 */
+	@PutMapping("/election/modifyPolicy")
+	public ResponseEntity<Policy> modifyPolicy(@RequestBody Policy policy){
+		Election election = electionService.findElectionById(policy.getElectionID());
+	  
+		if (election == null) {
+			return new ResponseEntity<Policy>(HttpStatus.NOT_FOUND);
+		}
+
+		electionService.modifyPolicy(policy);
+		return new ResponseEntity<Policy>(HttpStatus.OK);
+	}
+	
+	/**
+	 * getVotesByVoter () - View Number of votes by user for an election
+	 * @param electionID
+	 * @param userID
+	 * @return int number of votes
+	**/
+	@GetMapping("/election/getVotesByVoter/{electionID}/{userID}")
+	public int getVotesByVoter(@PathVariable("electionID") int electionID, @PathVariable("userID") String userID) {
+		int numVotes = 0;
+		Election election = electionService.findElectionById(electionID);
+		if (election == null) {
+			return numVotes;
+		}
+		numVotes = electionService.getVotesByVoter(electionID, userID);
+		return numVotes;
+	}
+
+	/**
+	 * getCandidateVotes() - Get number of votes for every candidate by candidate
+	 * @param electionID
+	 * @return HashMap<String,Integer>
+	 */
+	@GetMapping("/election/getCandidateVotes/{electionID}")
+	public HashMap<String, Integer> getCandidateVotes(@PathVariable("electionID") int electionID) {
+		HashMap<String,Integer> map =
+				new HashMap<String, Integer>();
+
+		Election election = electionService.findElectionById(electionID);
+
+		if (election == null) {
+			return  map;
+		}
+
+		map = electionService.tallyVotes(electionID);
+
+		return map;
+	}
+
+	/**
+	 * checkCloseElection() - Checks if an election should be set to closed.
+	 * @param electionID
+	 * @throws Exception
+	 */
+	@GetMapping("/election/checkCloseElection/{electionID}")
+	public void checkCloseElection(@PathVariable("electionID") int electionID) throws Exception {
+		electionService.calculateClosed(electionID);
+	}
+
+
+}
---
+++ ElectionHelperDao.java
@@ -0,0 +1,311 @@
+/*---------------------------------------------------------------------
+|  Class ElectionHelperDao
+|
+|  Purpose: Election Database Queries
+|
+|  Methods: 
+|
+|  Version: Sprint 3
+|  
+*-------------------------------------------------------------------*/
+
+package src.main.java.com.okta.springbootvue.SpringBootVueApplication.Dao;
+
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.Election;
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * ElectionHelperDao Class - Connects to MySQL database vision-database and performs queries through methods to update
+ * database.
+ */
+public class ElectionHelperDao {
+	src.main.java.com.okta.springbootvue.SpringBootVueApplication.Dao.ConnectionDao connectionDao = new src.main.java.com.okta.springbootvue.SpringBootVueApplication.Dao.ConnectionDao();
+	src.main.java.com.okta.springbootvue.SpringBootVueApplication.Dao.ElectionConnectionDao electionConnectionDao = new src.main.java.com.okta.springbootvue.SpringBootVueApplication.Dao.ElectionConnectionDao();
+	
+	/**
+	 * tallyVotes() - 
+	 * @param 
+	 * @return 
+	 */
+	public HashMap<String, Integer> tallyVotes(int electionID){
+	    //Display Votes in a Hash Map
+	    HashMap<String,Integer> map = 
+                new HashMap<String, Integer>(); 
+	    String can = "";
+	    String name = "";
+		
+	    try {
+		Connection conn = connectionDao.RetrieveConnection();
+		String sql = "SELECT * FROM ballot WHERE electionID=? AND canID<>?"; 
+		PreparedStatement stmt=conn.prepareStatement(sql); 
+		stmt.setInt(1,electionID);
+		stmt.setString(2,"Abstain");
+		 
+		ResultSet rs=stmt.executeQuery();
+		while(rs.next()) {
+		    can = rs.getString(4);
+		    name = rs.getString(5) + " " + rs.getString(6);
+		    //If Candidate exists increment vote count
+		    //Otherwise add new entry
+		    if(map.keySet().contains(name)){
+		        map.put(name, map.get(name) + 1); 
+		    }else{
+		        map.put(name, 1);
+		    }
+		}	
+		connectionDao.ReleaseConnection(conn);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	    return map;	
+	}
+	
+	/**
+	 * calculateLead() - 
+	 * @param 
+	 * @return 
+	 */
+	public ArrayList<String> calculateLead(int electionID){
+	    //Return Array in case of Tie
+	    ArrayList<String> winners = new ArrayList<String>();
+	    //Returned List of Candidate Counts
+	    HashMap<String,Integer> candidates = 
+                new HashMap<String, Integer>(); 
+	    candidates = tallyVotes(electionID);
+		
+	    //Loop through HashMap to Calculate Lead
+	    int leadNum = 0;
+	    String winner = "";
+	    for(HashMap.Entry<String,Integer> entry : candidates.entrySet())
+	    {
+                String key = entry.getKey();
+		Integer val = entry.getValue();
+		//If new largest number of votes found
+		//Clear current list of winners and start new list
+		if(val > leadNum)
+		{
+		    winners.clear();
+		    leadNum = val;
+		    winner = key;
+		    winners.add(winner);
+		}else if(val == leadNum){
+		    //If number of total votes match current lead
+		    //Add to winner ArrayList
+		    winners.add(winner);
+		}
+	    }
+	    return winners;
+	}
+	
+	/**
+	 * tallyType() - Resturn list of users who abstained or wrote in for given election
+	 * Can also returns users who voted for given candidate
+	 * @param 
+	 * @return 
+	 */
+	public ArrayList<User> tallyType(int electionID, String type){
+	    ArrayList<User> users = new ArrayList<User>();
+		
+	    try {
+		Connection conn = connectionDao.RetrieveConnection();
+		String sql = "SELECT * FROM ballot WHERE electionID=? AND canID=?"; 
+		PreparedStatement stmt=conn.prepareStatement(sql); 
+		stmt.setInt(1,electionID);
+		stmt.setString(2,type);
+		 
+		ResultSet rs=stmt.executeQuery();
+		src.main.java.com.okta.springbootvue.SpringBootVueApplication.Dao.UserConnectionDao userCon = new src.main.java.com.okta.springbootvue.SpringBootVueApplication.Dao.UserConnectionDao();
+		User user = new User();
+		//Loop through Users Returned
+		while(rs.next()) {
+			//Gather user data by userID
+			user = userCon.getUserById(rs.getString(2));
+			//Add user object to list to be returned
+			users.add(user);
+		}	
+		connectionDao.ReleaseConnection(conn);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	    return users;	
+	}
+	
+	/**
+	 * getDemographics() - Returns HashMap of User Demographics
+	 * @param 
+	 * @return 
+	 */
+	public HashMap<String, Integer> getDemographics(ArrayList<User> users){
+	    HashMap<String, Integer> demo = 
+                new HashMap<String, Integer>(); 
+	    int total = users.size();
+	
+	    //Demographic Counts
+	    //Gender
+            int male = 0, female = 0, notDisclosed = 0;
+	    //Race
+	    int nativeA = 0, asian = 0, black = 0, nativeH = 0, white = 0;
+	    //Ethnicity
+	    int hispanic = 0, notHispanic = 0;
+	    User curUser = new User();
+	    int i = 0;
+            //Loop through ArrayList of Users
+	    for(User user : users){
+		  curUser = users.get(i);
+		  i++;
+		  //Gender
+		  switch(curUser.getGender()){
+			  case "Male":
+				  male++;
+			  case "Female":
+				  female++;
+			  default:
+				  notDisclosed++;
+		  }
+	          
+		  //Race
+		    switch(curUser.getRace()){
+			    case "American Indian or Alaska Native":
+				    nativeA++;
+			    case "Asian":
+				    asian++;
+			    case "Black or African American":
+				    black++;
+			    case "Native Hawaiin or Other Pacific Islander":
+				    nativeH++;
+			    case "White":
+				    white++;
+			    default:
+				    System.out.println("No Race");
+		    }
+		  //Ethnicity
+		    switch(curUser.getEthnicity()){
+			    case "Hispanic or Latino or Spanish Origin":
+				    hispanic++;
+			    case "Not Hispanic or Latino or Spanish Origin":
+				    notHispanic++;
+			    default:
+				    System.out.println("No Ethnicity");
+		    }
+	    }
+	    //Calculate Results
+	    if(total>0){
+		//Gender
+	        demo.put("Male", (male/total));
+	        demo.put("Female", (female/total));
+	        demo.put("NA", (notDisclosed/total));
+		//Race
+		demo.put("NativeAmerican", (nativeA/total));
+	        demo.put("Asian", (asian/total));
+	        demo.put("Black", (black/total));
+		demo.put("NativeHawaiian", (nativeH/total));
+	        demo.put("White", (white/total));
+		//Ethnicity
+		demo.put("Hispanic", (hispanic/total));
+	        demo.put("NotHispanic", (notHispanic/total));
+	    }	
+		
+	    return demo;	
+	}
+
+
+	/**
+	 * calculateClosed() - Calculates if an election has elapsed it's closing date and time. Sets an election to 'closed'
+	 * if current date and time are past close date and time.
+	 * @param electionID
+	 * @throws Exception
+	 */
+	public void calculateClosed(int electionID) throws Exception{
+		Boolean closed = false;
+		String closeDate = "";
+		String closeTime = "";
+		Election election = new Election();
+		List<Election> electionList = new ArrayList<>();
+
+		try {
+			Connection conn = connectionDao.RetrieveConnection();
+			String sql = "SELECT * FROM election WHERE electionID=?";
+			PreparedStatement stmt=conn.prepareStatement(sql);
+			//stmt.setInt(1,electionID);
+			stmt.setInt(1,electionID);
+
+			ResultSet rs=stmt.executeQuery();
+			while(rs.next()) {
+				election.setElectionID(rs.getInt(1));
+				election.setTitle(rs.getString(2));
+				election.setClosed(rs.getInt(3));
+				election.setClose_date(rs.getString(4));
+				election.setClose_time(rs.getString(5));
+				election.setNum_candidates(rs.getInt(6));
+				election.setNum_votes(rs.getInt(7));
+				election.setStart_date(rs.getString(8));
+				election.setStart_time(rs.getString(9));
+				election.setDescription(rs.getString(10));
+				election.setElection_key(rs.getString(11));
+			}
+			connectionDao.ReleaseConnection(conn);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		//Assign closeDate the election's closing date
+		closeDate = election.getClose_date();
+
+		//Assign closeTime the election's closing time
+		closeTime = election.getClose_time();
+
+		//Formatters for date and time
+		SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+		SimpleDateFormat timeFormatter = new SimpleDateFormat("HH:mm:ss");
+
+		//Parse election close date to adhere to date format
+		Date formattedCloseDate=dateFormatter.parse(closeDate);
+
+		//Parse election close time to adhere to the time format
+		Date formattedCloseTime=timeFormatter.parse(closeTime);
+
+		//Get the current date
+		LocalDate nowDate = LocalDate.now();
+
+		//Get today's date as a Date
+		Date currentDate = Date.from(nowDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+		//Get the current time
+		//LocalTime nowTime = LocalTime.now();
+
+		//Get the current time in an instant
+		Instant instant = Instant.now();
+
+// get overall time
+		LocalTime time = instant.atZone(ZoneOffset.UTC).toLocalTime();
+// get hour
+		int hour = instant.atZone(ZoneOffset.UTC).getHour();
+// get minute
+		int minute = instant.atZone(ZoneOffset.UTC).getMinute();
+// get second
+		int second = instant.atZone(ZoneOffset.UTC).getSecond();
+
+		String currentTime = hour + ":" + minute + ":" + second;
+
+		Date formattedCurrentTime=timeFormatter.parse(currentTime);
+
+		//If past election close date, close election. If on election close date and after close time, close election
+		if (currentDate.after(formattedCloseDate)) {
+			election.setClosed(1);
+			electionConnectionDao.updateElection(election,electionList);
+		} else if(currentDate.equals(formattedCloseDate) && formattedCurrentTime.after(formattedCloseTime)) {
+			election.setClosed(1);
+			electionConnectionDao.updateElection(election,electionList);
+		}
+	}
+}

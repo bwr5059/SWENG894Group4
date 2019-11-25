dc0fcc193355986df2388cef13bbd108c579922e
bwr5059 <55011215+bwr5059@users.noreply.github.com>
2019-11-24 19:33:01 -0500

    Updated viewQuestions endpoint

--- SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Controller/QuestionController.java
+++ SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Controller/QuestionController.java
@@ -11,16 +11,14 @@
 package src.main.java.com.okta.springbootvue.SpringBootVueApplication.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.Candidate;
-import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.Question;
 import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Service.CandidateService;
 import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Service.QuestionService;
 
+import java.util.HashMap;
 import java.util.List;
 /**
  * Class QuestionController - Returns question object and data using CRUD methods.
@@ -40,15 +38,16 @@ public class QuestionController {
 	 * @return
 	 */
 	@GetMapping("/question/viewQuestions/{canID}")
-	public ResponseEntity<List<Question>> listQuestionsByCandidate(@PathVariable("canID") String canID){ 
+	public List<HashMap> listQuestionsByCandidate(@PathVariable("canID") String canID){
 		Candidate candidate = candidateService.findById(canID);
 	  
 		if (candidate == null) {
-			return new ResponseEntity<List<Question>>(HttpStatus.NOT_FOUND);
+			//return new ResponseEntity<List<Question>>(HttpStatus.NOT_FOUND);
 		}
 
-		List<Question> questions = questionService.viewQuestionsByCandidate(canID);
-		return new ResponseEntity<List<Question>>(questions, HttpStatus.OK);
+		List<HashMap> questions = questionService.viewQuestionsByCandidate(canID);
+
+		return questions;
 	}
   
 }

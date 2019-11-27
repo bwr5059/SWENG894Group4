4c393c6718f80abd434a2f4e69df1c396ad9d60e
baileyk2 <46055645+baileyk2@users.noreply.github.com>
2019-11-27 09:08:25 -0500

    Merge Brent Changes

--- SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Controller/QuestionController.java
+++ SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Controller/QuestionController.java
@@ -10,33 +10,16 @@
 *-------------------------------------------------------------------*/
 package src.main.java.com.okta.springbootvue.SpringBootVueApplication.Controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
-
+import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.Candidate;
 import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Service.CandidateService;
 import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Service.QuestionService;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.Question;
-import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.Candidate;
+import java.util.HashMap;
+import java.util.List;
 /**
  * Class QuestionController - Returns question object and data using CRUD methods.
  */
@@ -55,15 +38,16 @@ public class QuestionController {
 	 * @return
 	 */
 	@GetMapping("/question/viewQuestions/{canID}")
-	public ResponseEntity<List<Question>> listQuestionsByCandidate(@PathVariable("canID") String canID){ 
+	public List<HashMap> listQuestionsByCandidate(@PathVariable("canID") String canID){
 		Candidate candidate = candidateService.findById(canID);
 	  
 		if (candidate == null) {
-			return new ResponseEntity<List<Question>>(HttpStatus.NOT_FOUND);
+			//return new ResponseEntity<List<Question>>(HttpStatus.NOT_FOUND);
 		}
 
-		List<Question> questions = questionService.viewQuestionsByCandidate(canID);
-		return new ResponseEntity<List<Question>>(questions, HttpStatus.OK);
+		List<HashMap> questions = questionService.viewQuestionsByCandidate(canID);
+
+		return questions;
 	}
   
 }

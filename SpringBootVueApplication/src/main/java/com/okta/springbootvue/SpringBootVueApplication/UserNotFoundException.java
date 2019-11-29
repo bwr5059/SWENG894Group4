f7dd23af48f7b4d5478075033a43c5a0982a4b8d
bwr5059 <55011215+bwr5059@users.noreply.github.com>
2019-11-29 15:10:07 -0500

    Clearing Branch

--- SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package src.main.java.com.okta.springbootvue.SpringBootVueApplication;
-
-
-class UserNotFoundException extends RuntimeException {
-
-  UserNotFoundException(Long id) {
-    super("Could not find user " + id);
-  }
-}

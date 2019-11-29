7ec7ebfbdc2dad214fdd37a5f09cd5afefcd415a
bwr5059 <55011215+bwr5059@users.noreply.github.com>
2019-11-29 15:11:17 -0500

    Clearing Branch

--- SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/ElectionNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package src.main.java.com.okta.springbootvue.SpringBootVueApplication;
-
-
-class ElectionNotFoundException extends RuntimeException {
-
-  ElectionNotFoundException(Long electionId) {
-    super("Could not find election " + electionId);
-  }
-}

52cf11b9f262399f8e5826e96a0cb97eca30437d
baileyk2 <46055645+baileyk2@users.noreply.github.com>
2019-12-06 08:28:55 -0500

    Comments

--- SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Controller/CandidateController.java
+++ SpringBootVueApplication/src/main/java/com/okta/springbootvue/SpringBootVueApplication/Controller/CandidateController.java
@@ -3,10 +3,7 @@
 |
 |  Purpose: Set up Candidate REST Endpoints
 |
-|  Methods: listAllCandidates, getCandidate, getCandidateName, newCandidate,
-|           modifyCandidate
-|
-|  Version: Sprint 1
+|  Version: Sprint 3
 |  
 *-------------------------------------------------------------------*/
 
@@ -26,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,7 +30,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-//import src.main.java.com.okta.springbootvue.SpringBootVueApplication;
 
 import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.Candidate;
 import src.main.java.com.okta.springbootvue.SpringBootVueApplication.Model.Election;
